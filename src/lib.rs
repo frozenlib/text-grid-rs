dacfe055560fce72b8571ce4a215110e844eae8e
frozenlib <frozenlib@users.noreply.github.com>
2018-12-17 01:44:06 +0900

    Set `doc(html_root_url)` attribute.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 //!
 //! See [`Grid`] (High Level API) or [`GridBuf`] (Low Level API) for details.
 
+#![doc(html_root_url = "https://docs.rs/text-grid/0.1.0")]
+
 mod cell;
 mod grid;
 mod grid_buf;
