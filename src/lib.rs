a4b2e67115c73d0468890943e00febb28bb03e28
frozenlib <frozenlib@users.noreply.github.com>
2019-02-06 10:15:55 +0900

    version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! A library to create formatted plain-text tables.
 //!
 //! See [`Grid`] (High Level API) or [`GridBuf`] (Low Level API) for details.
-#![doc(html_root_url = "https://docs.rs/text-grid/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/text-grid/0.2.0")]
 
 mod cell;
 mod grid;
