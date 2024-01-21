7a8bc6351f731158663d64d1b8f9d873781364ab
frozenlib <frozenlib@users.noreply.github.com>
2024-01-21 12:32:28 +0900

    Remove `html_root_url`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! A library to create formatted plain-text tables.
 //!
 //! See [`Grid`] (High Level API) or [`GridBuilder`] (Low Level API) for details.
-#![doc(html_root_url = "https://docs.rs/text-grid/0.2.0")]
 
 mod cell;
 mod cells;
