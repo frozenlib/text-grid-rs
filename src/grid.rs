1d50b601f0e44e1aeaa1cc37ec1c81e82ab00d79
frozenlib <frozenlib@users.noreply.github.com>
2024-01-30 20:42:59 +0900

    Change parameter names.

--- src/grid.rs
+++ src/grid.rs
@@ -29,40 +29,40 @@ use crate::{grid_builder::*, Cells, CellsSchema, CellsSchemaExt, DefaultCellsSch
 ///    2 | 200 |
 /// "#);
 /// ```
-pub fn to_grid(iter: impl IntoIterator<Item = impl Cells>) -> String {
-    to_grid_with_schema(iter, DefaultCellsSchema::default())
+pub fn to_grid(rows: impl IntoIterator<Item = impl Cells>) -> String {
+    to_grid_with_schema(rows, DefaultCellsSchema::default())
 }
 pub fn to_grid_with_schema<T>(
-    iter: impl IntoIterator<Item = T>,
+    rows: impl IntoIterator<Item = T>,
     schema: impl CellsSchema<Source = T>,
 ) -> String {
-    GridBuilder::from_iter_with_schema(iter, &schema).to_string()
+    GridBuilder::from_iter_with_schema(rows, &schema).to_string()
 }
 pub fn to_grid_with_schema_ref<'a, T: 'a>(
-    iter: impl IntoIterator<Item = &'a T>,
+    rows: impl IntoIterator<Item = &'a T>,
     schema: impl CellsSchema<Source = T>,
 ) -> String {
-    GridBuilder::from_iter_with_schema(iter, &schema.map_ref()).to_string()
+    GridBuilder::from_iter_with_schema(rows, &schema.map_ref()).to_string()
 }
 
-pub fn to_csv(iter: impl IntoIterator<Item = impl Cells>) -> String {
-    to_csv_with_schema(iter, DefaultCellsSchema::default())
+pub fn to_csv(rows: impl IntoIterator<Item = impl Cells>) -> String {
+    to_csv_with_schema(rows, DefaultCellsSchema::default())
 }
 pub fn to_csv_with_schema<T>(
-    iter: impl IntoIterator<Item = T>,
+    rows: impl IntoIterator<Item = T>,
     schema: impl CellsSchema<Source = T>,
 ) -> String {
     let mut bytes = Vec::new();
     {
         let mut csv_writer = csv::Writer::from_writer(&mut bytes);
-        write_csv(&mut csv_writer, iter, &schema, ".").unwrap();
+        write_csv(&mut csv_writer, rows, &schema, ".").unwrap();
         csv_writer.flush().unwrap();
     }
     String::from_utf8(bytes).unwrap()
 }
 pub fn to_csv_with_schema_ref<'a, T: 'a>(
-    iter: impl IntoIterator<Item = &'a T>,
+    rows: impl IntoIterator<Item = &'a T>,
     schema: impl CellsSchema<Source = T>,
 ) -> String {
-    to_csv_with_schema(iter, schema.map_ref())
+    to_csv_with_schema(rows, schema.map_ref())
 }
